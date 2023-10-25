9137833df0adf33edb1b12d46a9e2d1b7dd89d35
m1k0t0f667 <h.briffaux@gmail.com>
2023-10-25 17:15:25 +0200

    Added mutiple files

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,36 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command, command.data.toJSON());
+        console.log(
+          `Command : ${command.data.name} has been passed through the handler`
+        );
+      }
+    }
+    const clientId = "1166698840930402304";
+    const guildId = "1087404027194642442";
+    const rest = new REST({ version: "9" }).setToken(process.env.token);
+    try {
+      console.log("Started refreshing application (/) commands.");
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commandArray,
+      });
+      console.log("Successfully reload application (/)");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
